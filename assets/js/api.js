4ab92e31e6fbfbdf7c8acf500b96adb8aec0af2b
EvanildoBatista <evanildo.batista@gmail.com>
2025-08-31 21:51:12 -0300

    Fix: Changed the profile URL

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/EvanildoBatista/js-developer-portfolio/refs/heads/develop/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
